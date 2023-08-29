a029f6df7abfd2a589cdc5b846b26a3ef0d54fd8
rpsobolewski <rpsobolewski@gmail.com>
2023-08-29 18:53:56 +0200

    initial commit 1

---
+++ jssnack1/assets/js/main.js
@@ -0,0 +1,8 @@
+//L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
+
+
+const numero1 = prompt("inserire il primo numero");
+const numero2 = prompt("inserire il secondo numero");
+
+console.log(numero1, numero2);
+
