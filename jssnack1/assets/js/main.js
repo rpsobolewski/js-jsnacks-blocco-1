ab75918c9c8b60ee0d3db20dd152120b17542814
rpsobolewski <rpsobolewski@gmail.com>
2023-08-29 19:06:55 +0200

    fine esercizio 1

--- jssnack1/assets/js/main.js
+++ jssnack1/assets/js/main.js
@@ -5,4 +5,16 @@ const numero1 = prompt("inserire il primo numero");
 const numero2 = prompt("inserire il secondo numero");
 
 console.log(numero1, numero2);
+const risultato = document.getElementById("risultato")
+if(numero1 > numero2){
+    const maggiore = numero1;
+    console.log(maggiore);
+    risultato.innerHTML = "il primo numero è maggiore";
+} else if (numero1 < numero2){
+    const maggiore = numero2;
+    console.log(maggiore);
+    risultato.innerHTML = "il secondo numero è maggiore";
+} else {
+    risultato.innerHTML = "pareggio";
+}
 
