9f6bb4094bf458bc75c0bf53e98ad83734fda36a
rpsobolewski <rpsobolewski@gmail.com>
2023-08-30 11:48:25 +0200

    using while

--- jssnack2/assets/js/main.js
+++ jssnack2/assets/js/main.js
@@ -1,7 +1,7 @@
 //L’utente inserisce due parole in successione, con due prompt.
 //Il software stampa prima la parola più corta, poi la parola più lunga.
 
-const parola1 = prompt("inserire la prima parola");
+/* const parola1 = prompt("inserire la prima parola");
 const parola2 = prompt("inserire la seconda parola");
 
 console.log(parola1, parola2);
@@ -23,6 +23,35 @@ if (lunghezza1 > lunghezza2) {
 } else if (lunghezza1 < lunghezza2){
     parolaLunga.innerHTML = parola2;
     parolaCorta.innerHTML = parola1;
+} else{
+    parolaCorta.innerHTML = "le parole hanno la stessa lunghezza"
+} */
+
+let i = 0;
+const parole = []
+while (i<2) {
+
+    let parola = prompt("digita una parola");
+    parole.push(parola);
+    i++;
+
+}
+console.log(parole);
+
+const parolaLunga = document.getElementById("parolaLunga");
+const parolaCorta = document.getElementById("parolaCorta");
+
+const lunghezza1 = parole[0].length;
+const lunghezza2 = parole[1].length;
+
+
+if (lunghezza1 > lunghezza2) {
+    parolaLunga.innerHTML = parole[0];
+    parolaCorta.innerHTML = parole[1];
+
+} else if (lunghezza1 < lunghezza2){
+    parolaLunga.innerHTML = parole[1];
+    parolaCorta.innerHTML = parole[0];
 } else{
     parolaCorta.innerHTML = "le parole hanno la stessa lunghezza"
 }
